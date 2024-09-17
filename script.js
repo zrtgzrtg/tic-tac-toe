7d93851aa5715177b7f4cab39f8c75dcdc065440
zrtgChromebook <thomas4stank@gmail.com>
2024-09-17 04:54:32 +0200

    Finished Game funcitonality
    Need to clean up layout/ add player names/restart button/visual
    indictation that games has ended and who has won

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 const game = (function() {
 
     let gameboard;
+    const containerHtml = document.querySelector(".container")
+    let playerXturn = true
+
     function init() {
         gameboard = Array(9).fill().map(() => new squareState())
+        genDOM()
+        bindListener()
     }
     function getGameboard(){
         return gameboard
@@ -13,7 +18,7 @@ const game = (function() {
         if(square.getState() === "empty") {
             square.changeState(playerSymbol)
         } else {
-            throw Error ("Square Already filled!!")
+              throw Error("Square Already filled!!")
         }
         evaluateGame(playerSymbol)
     }
@@ -23,9 +28,12 @@ const game = (function() {
             let res = evalWinOption(winOptions[i],playerSymbol)
             if(res === true) {
                 console.log("Game Finished. Player with Symbol: " + playerSymbol + " won!!")
+                return
             }
-            
         }
+            if(checkTie()){
+                console.log("Game Finished. No more Squares available!")
+            }
     }
     function evalWinOption(arr3val,playerSymbol) {
         if(gameboard[arr3val[0]].getState()===gameboard[arr3val[1]].getState()){
@@ -34,6 +42,43 @@ const game = (function() {
         } else {
             return false
         }
+    }
+    function checkTie(){
+        let res = true
+        gameboard.forEach(element => {
+            if(element.getState()==="empty"){
+                res = false
+            }
+        });
+            return res
+    }
+    function genDOM() {
+        for(let i = 0;i<gameboard.length;i++) {
+            const square = document.createElement("div")
+            square.setAttribute("class", "game-square")
+            square.setAttribute("id", `${i}`)
+            containerHtml.appendChild(square)
+    
+        }
+    }
+    function updateDOM(playerXturn, eventid) {
+        const children = containerHtml.children
+        let svgString = playerXturn ? "svg/x.svg" : "svg/circle.svg"
+        let svgPic = document.createElement("img")
+        svgPic.setAttribute("src", svgString)
+        let res =(gameboard[eventid].getState()==="empty") ? null : svgPic 
+            if(res !== null) {
+            children[eventid].appendChild(res) 
+            }
+        }
+    function bindListener() {
+        
+        containerHtml.addEventListener("click",(event) => {
+            let playerSymbol = playerXturn ? "x" : "o"
+            changeSquare(event.target.id, playerSymbol)
+            updateDOM(playerXturn, event.target.id)
+            playerXturn = !playerXturn
+        })
     }
     return {
         init,
